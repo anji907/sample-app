174ce28a3ed48fc227e732ef6f882fa70e76c9af
anji907 <anji.sk129@gmail.com>
2023-09-21 11:01:52 +0900

    リンクに対するテスト

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "laytout links" do
+    get root_path
+    assert_template "static_pages/home"
+    assert_select "a[href=?]", root_path, count: 2 # 2 links to root_path
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+  end
+end
