ee8abd3baa93cc7ae48fadda6235fcbab201bb47
anji907 <anji.sk129@gmail.com>
2023-09-27 10:52:07 +0900

    ログイン、ログアウトに関するルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 end
