49920cdd5be87ee2b36494b32f6d795dd60ee156
anji907 <anji.sk129@gmail.com>
2023-10-01 21:55:06 +0900

    フレンドリーフォワーディングを実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,17 @@ class SessionsController < ApplicationController
   # destroyアクションに対してはCSRFトークンの検証をスキップする
   protect_from_forgery except: :destroy
 
-  def new
-  end
+  def new; end
 
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
+      # セッション固定攻撃から保護する
       reset_session
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       log_in(@user)
-      redirect_to @user
+      redirect_to forwarding_url || @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render :new, status: :unprocessable_entity
