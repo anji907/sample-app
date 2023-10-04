543a7c12d3fef63bb333e0e99dc83b4215f7352c
anji907 <anji.sk129@gmail.com>
2023-10-04 19:40:48 +0900

    テストコードの追加

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -31,6 +31,7 @@ class ActionDispatch::IntegrationTest
   # テストユーザーとしてログインする
   def log_in_as(user, password: "password", remember_me: "1")
     post login_path, params: { session: { email: user.email,
-                                          password: password} }
+                                          password: password,
+                                          remember_me: remember_me} }
   end
 end
