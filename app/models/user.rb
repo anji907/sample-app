7df04b803e914fa6a3b1a4e2157033e4468f931f
anji907 <anji.sk129@gmail.com>
2023-10-09 14:45:30 +0900

    feedメソッドをリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,8 +77,11 @@ class User < ApplicationRecord
   end
 
   def feed
-    # following_idsはhas_many :followingにより自動的に生成されるメソッド
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+                     .includes(:user, image_attachment: :blob)
   end
 
   def follow(user)
